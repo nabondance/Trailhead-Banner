2c3d42923aff1abd29979c126091396736021f36
Nathan Abondance <nabondance@meilleursagents.com>
2024-12-23 17:07:22 +0100

    Add TrailheadBanner component and update page layout

---
+++ src/app/TrailheadBanner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TrailheadBanner = () => {
+  return (
+    <div className="trailhead-banner bg-blue-500 text-white p-4 text-center">
+      <h1>Welcome to Trailhead-Banner</h1>
+      <p>Hop Hop Hop.</p>
+    </div>
+  );
+};
+
+export default TrailheadBanner;
