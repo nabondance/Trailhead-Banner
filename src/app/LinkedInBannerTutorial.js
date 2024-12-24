bec5a89f8466f888385a73860cc33002aed81949
Nathan Abondance <nabondance@meilleursagents.com>
2024-12-24 18:42:54 +0100

    fix(tutorial): Simplify instructions for updating LinkedIn banner image

--- src/app/LinkedInBannerTutorial.js
+++ src/app/LinkedInBannerTutorial.js
@@ -7,11 +7,10 @@ const LinkedInBannerTutorial = () => {
       <ol>
         <li>Log in to your LinkedIn account.</li>
         <li>Go to your profile by clicking on your profile picture or name.</li>
-        <li>Click on the pencil icon in the top right corner of your profile header.</li>
-        <li>In the Edit Intro pop-up, click on the pencil icon on the banner image.</li>
+        <li>Click on the pencil icon on the banner image.</li>
         <li>Click on "Change photo" and upload the new banner image you have generated.</li>
         <li>Adjust the positioning of the image if necessary and click "Apply".</li>
-        <li>Click "Save" to update your profile with the new banner image.</li>
+        <li>Click "Save".</li>
       </ol>
     </div>
   );
