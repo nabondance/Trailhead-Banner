abb6099d3741f74c479f7ebafb0cbe261084099c
Nathan Abondance <nabondance@meilleursagents.com>
2024-12-25 22:14:39 +0100

    feat(TrailheadBanner): Add logo and improve header layout

--- src/app/TrailheadBannerHeader.js
+++ src/app/TrailheadBannerHeader.js
@@ -1,8 +1,15 @@
+import Image from 'next/image';
+
 const TrailheadBannerHeader = () => {
   return (
     <div className='trailhead-banner-header'>
-      <h1>Welcome to Trailhead-Banner</h1>
-      <p>Generate your LinkedIn Banner with your Trailhead data</p>
+      <div className='logo-container'>
+        <Image src='/assets/trailhead-banner-logo.svg' alt='Trailhead-Banner Logo' width={50} height={50} />
+      </div>
+      <div className='header-content'>
+        <h1>Welcome to Trailhead-Banner</h1>
+        <p>Generate your LinkedIn Banner with your Trailhead data</p>
+      </div>
     </div>
   );
 };
