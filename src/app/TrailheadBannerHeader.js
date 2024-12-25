51650a81cf5d5fdfaa7b9e9f5645931267ca64fe
Nathan Abondance <nabondance@meilleursagents.com>
2024-12-25 14:07:36 +0100

    feat(styles): Add modern gradient background and styling to TrailheadBannerHeader component

--- src/app/TrailheadBannerHeader.js
+++ src/app/TrailheadBannerHeader.js
@@ -1,10 +1,8 @@
-import React from 'react';
-
 const TrailheadBannerHeader = () => {
   return (
-    <div className='trailhead-banner-header bg-blue-500 p-4 text-center text-white'>
+    <div className='trailhead-banner-header'>
       <h1>Welcome to Trailhead-Banner</h1>
-      <p>Generate your Linkedin Banner with your Trailhead data</p>
+      <p>Generate your LinkedIn Banner with your Trailhead data</p>
     </div>
   );
 };
