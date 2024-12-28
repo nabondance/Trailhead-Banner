40a4ace6919fd0115c57b0d7694b8caa5094df19
Nathan Abondance <nabondance@meilleursagents.com>
2024-12-28 23:48:09 +0100

    feat(BackgroundLibraryPage): WIP

--- src/components/BackgroundLibraryPage.js
+++ src/components/BackgroundLibraryPage.js
@@ -1,20 +1,58 @@
-import React from 'react';
+'use client';
+
+import React, { useState } from 'react';
+import Image from 'next/image';
 import '../styles/globals.css';
 
+const BannerCard = ({ src, alt, description, onClick }) => (
+    <div className='example-card'>
+      <Image
+        src={src}
+        alt={alt}
+        width={600}
+        height={400}
+        onClick={() => onClick(src)}
+      />
+      <p>{description}</p>
+    </div>
+  );
+
 const BackgroundLibraryPage = () => {
+    const [fullscreenImage, setFullscreenImage] = useState(null);
+
+    const handleImageClick = (src) => {
+        setFullscreenImage(src);
+      };
+
+      const handleOverlayClick = () => {
+        setFullscreenImage(null);
+      };
+
+    const banners = [
+        { src: '/assets/banner-library/banner1.png', alt: 'Banner 1', description: 'Banner 1' },
+      ];
+
   return (
     <div className='background-library-container'>
       <h1>Background Library</h1>
-      <p>Choose from a variety of backgrounds for your LinkedIn banner</p>
-      <div className='background-library'>
-        <img src='/path/to/example1.png' alt='Example 1' />
-        <p>Description for Example 1</p>
+      <h2>WIP</h2>
+      <div className='library-grid'>
+        {banners.map((example, index) => (
+          <BannerCard
+            key={index}
+            src={example.src}
+            alt={example.alt}
+            description={example.description}
+            onClick={handleImageClick}
+          />
+        ))}
       </div>
-      <div className='background-library'>
-        <img src='/path/to/example2.png' alt='Example 2' />
-        <p>Description for Example 2</p>
-      </div>
-      {/* Add more examples as needed */}
+
+      {fullscreenImage && (
+        <div className='fullscreen-overlay visible' onClick={handleOverlayClick}>
+          <Image src={fullscreenImage} alt='Full Screen Example' layout='fill' objectFit='contain' />
+        </div>
+      )}
     </div>
   );
 };
