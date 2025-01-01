9a08fdad3322118041fb64dfe5405406e4dc4c11
Nathan Abondance <nabondance@meilleursagents.com>
2025-01-01 19:30:04 +0100

    refactor(TrailheadBannerHeader): simplify header title for clarity

--- src/components/TrailheadBannerHeader.js
+++ src/components/TrailheadBannerHeader.js
@@ -17,7 +17,7 @@ const TrailheadBannerHeader = () => {
         <Image src='/assets/logos/trailhead-banner-logo.svg' alt='Trailhead-Banner Logo' width={120} height={120} />
       </div>
       <div className='header-content'>
-        <h1>Welcome to Trailhead-Banner</h1>
+        <h1>Trailhead-Banner</h1>
         <h2>Generate your LinkedIn Banner with your Trailhead data</h2>
         <nav className='header-nav'>
           <Link href='/'>Banner Generator</Link>
