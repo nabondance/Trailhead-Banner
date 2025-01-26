82dce5fb4a405cb5f0cc386ca2b7a37712f3a17f
Nathan Abondance <nabondance@meilleursagents.com>
2025-01-26 17:26:29 +0100

    feat: refactor banner handling and update banner counter logic

--- src/pages/api/add-banner.js
+++ src/pages/api/add-banner.js
@@ -6,17 +6,32 @@ const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
-    const { th_username, thb_banner_hash, source_env } = req.body;
+    const { thb_data } = req.body;
 
-    if (!th_username || !thb_banner_hash) {
-      return res.status(400).json({ error: 'Missing th_username or thb_banner_hash' });
+    if (!thb_data.th_username) {
+      return res.status(400).json({ error: 'Missing th_username' });
     }
 
     try {
-      const { data, error } = await supabase.from('banners').insert([{ th_username, thb_banner_hash, source_env }]);
+      const { data, error } = await supabase.from('banners').insert([
+        {
+          th_username: thb_data.th_username,
+          source_env: process.env.VERCEL_ENV ? process.env.VERCEL_ENV : 'development',
+          thb_options: thb_data.thb_options,
+          thb_version: thb_data.thb_version,
+          thb_banner_hash: thb_data.bannerHash,
+          th_nb_points: thb_data.rankData.points,
+          th_nb_certif: thb_data.certificationsData.certifications.length,
+          th_nb_sb: thb_data.superbadgesData.earnedAwards.edges.length,
+          th_nb_badge: thb_data.rankData.badges,
+          th_certif: thb_data.certificationsData.certifications,
+          th_sb: thb_data.superbadgesData.earnedAwards.edges,
+          th_mvp: thb_data.mvp,
+        },
+      ]);
 
       if (error) {
-        throw error;
+        res.status(500).json({ error: 'Failed to add banner: ', error });
       }
 
       res.status(200).json({ message: 'Banner added successfully', data });
--- src/utils/supabaseUtils.js
+++ src/utils/supabaseUtils.js
@@ -1,23 +1,79 @@
-export const updateBannerCounter = async (username, bannerHash, protocol, host) => {
-  const baseUrl = process.env.VERCEL_URL
-    ? `https://${process.env.VERCEL_URL}` // Use Vercel's domain in production
-    : `${protocol}://${host}`; // Fallback for local development
+import packageJson from '../../package.json';
 
-  const addBannerUrl = `${baseUrl}/api/add-banner`;
-  const response = await fetch(addBannerUrl, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      th_username: username,
-      thb_banner_hash: bannerHash,
-      source_env: process.env.VERCEL_ENV ? process.env.VERCEL_ENV : 'development',
-    }),
-  });
+class SupabaseUtils {
+  static async updateBannerCounter(thb_data, protocol, host) {
+    const baseUrl = process.env.VERCEL_URL
+      ? `https://${process.env.VERCEL_URL}` // Use Vercel's domain in production
+      : `${protocol}://${host}`; // Fallback for local development
 
-  if (!response.ok) {
-    const errorData = await response.json();
-    throw new Error(errorData.error || 'Failed to save banner hash');
+    const addBannerUrl = `${baseUrl}/api/add-banner`;
+    const cleanedData = SupabaseUtils.cleanData(thb_data);
+    try {
+      const response = await fetch(addBannerUrl, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          thb_data: cleanedData,
+        }),
+      });
+
+      if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.error || 'Failed to save banner hash');
+      }
+    } catch (error) {
+      console.error('Error updating banner counter:', error);
+    }
   }
-};
+
+  static cleanData(data) {
+    // Clean the data as needed
+    const cleanedData = {
+      th_username: data.th_username,
+      thb_options: data.options,
+      thb_version: packageJson.version,
+      bannerHash: data.bannerHash,
+      mvp: data.mvpData.isMvp,
+      rankData: {
+        rank: data.rankData.rank.title,
+        points: data.rankData.earnedPointsSum,
+        badges: data.rankData.earnedBadgesCount,
+        trails: data.rankData.completedTrailCount,
+      },
+      certificationsData: {
+        certifications: data.certificationsData.certifications.map((cert) => ({
+          title: cert.title,
+          dateCompleted: cert.dateCompleted,
+          dateExpired: cert.dateExpired,
+          status: cert.status.title,
+          logoUrl: cert.logoUrl,
+          product: cert.product,
+        })),
+      },
+      superbadgesData: {
+        earnedAwards: {
+          edges: data.superbadgesData.earnedAwards.edges
+            .map((edge) => {
+              if (edge.node.award) {
+                return {
+                  node: {
+                    award: {
+                      title: edge.node.award.title,
+                      icon: edge.node.award.icon,
+                    },
+                  },
+                };
+              }
+              return null;
+            })
+            .filter((edge) => edge !== null),
+        },
+      },
+    };
+    return cleanedData;
+  }
+}
+
+export default SupabaseUtils;
