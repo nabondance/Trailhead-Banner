ac2b9f298dbf678708d1f9f24a48f263d11d996e
Nathan Abondance <nabondance@meilleursagents.com>
2025-02-10 17:13:19 +0100

    feat(sitemap): use standard Next.js sitemap generation

--- next-sitemap.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  siteUrl: 'https://thb.nabondance.me',
-  generateRobotsTxt: true, // Generate robots.txt file
-  sitemapSize: 5000, // Maximum number of URLs per sitemap file
-  changefreq: 'daily', // Change frequency for all URLs
-  priority: 0.7, // Default priority for all URLs
-  // Additional options...
-};
