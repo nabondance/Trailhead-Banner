af3e0dc7eb09ee145c51976181ae76f2ffd98411
Nathan Abondance <32196400+nabondance@users.noreply.github.com>
2025-03-31 17:14:04 +0200

    feat: add logging function for banner generation options

--- src/utils/dataUtils.js
+++ src/utils/dataUtils.js
@@ -13,3 +13,46 @@ export const getLocalCertificationData = (certification) => {
   }
   return certificationData;
 };
+
+export const logOptions = (options) => {
+  console.log('Generating banner with the following data:');
+  console.log('Username:', options.username);
+  console.log('Rank Data:', options.rankData);
+  console.log('Certifications Data:', options.certificationsData);
+  console.log('Badges Data:', options.badgesData);
+  console.log('Superbadges Data:', options.superbadgesData);
+  console.log('Background Options:', {
+    kind: options.backgroundKind,
+    libraryUrl: options.backgroundLibraryUrl,
+    customImageUrl: options.customBackgroundImageUrl,
+    backgroundColor: options.backgroundColor,
+  });
+  console.log('Display Options:', {
+    rankLogo: options.displayRankLogo,
+  });
+  console.log('Counter Options:', {
+    textColor: options.textColor,
+    badgeCount: options.displayBadgeCount,
+    superbadgeCount: options.displaySuperbadgeCount,
+    certificationCount: options.displayCertificationCount,
+    trailCount: options.displayTrailCount,
+    pointCount: options.displayPointCount,
+  });
+  console.log('Badge Options:', {
+    labelColor: options.badgeLabelColor,
+    messageColor: options.badgeMessageColor,
+  });
+  console.log('Superbadge Options:', {
+    superbadges: options.displaySuperbadges,
+    displayLastXSuperbadges: options.lastXSuperbadges,
+    lastXSuperbadges: options.lastXSuperbadges,
+  });
+  console.log('Certification Options:', {
+    includeExpired: options.includeExpiredCertifications,
+    includeRetired: options.includeRetiredCertifications,
+    displayLastXCertifications: options.lastXCertifications,
+    lastXCertifications: options.lastXCertifications,
+    certificationAlignment: options.certificationAlignment,
+  });
+  console.log('MVP Data:', options.mvpData);
+};
