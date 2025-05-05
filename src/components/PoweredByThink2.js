f1a317d3c0a57c5238896a054af20d635930f115
Nathan Abondance <32196400+nabondance@users.noreply.github.com>
2025-05-05 11:31:52 +0200

    feat(think2): use wallpaper as component background

--- src/components/PoweredByThink2.js
+++ src/components/PoweredByThink2.js
@@ -4,20 +4,20 @@ import Image from 'next/image';
 const PoweredByThink2 = () => {
   return (
     <div className='think2-container' onClick={() => window.open('https://think2.ai', '_blank')}>
-      <div className='think2-logo-container'>
+      <div className='think2-background'>
         <Image
-          src='/assets/logos/style_urls.svg'
-          //   src='/assets/logos/wallpaper-patterns.svg'
-          alt='Think2 Logo'
-          width={700}
-          height={550}
+          src='/assets/logos/wallpaper-patterns.svg'
+          alt='Think2 Background Pattern'
+          fill
           priority
-          className='think2-logo'
+          className='think2-pattern'
         />
       </div>
-      <p className='think2-description'>
-        Infrastructure costs graciously handled by <span className='think2-highlight'>Think2</span>
-      </p>
+      <div className='think2-content'>
+        <p className='think2-description'>
+          Infrastructure costs graciously handled by <span className='think2-highlight'>Think2</span>
+        </p>
+      </div>
     </div>
   );
 };
