accd23c7674176e67cd36f0faed64d8ac3c15426
Nathan Abondance <32196400+nabondance@users.noreply.github.com>
2026-09-22 20:49:12 +0200

    docs: simplify sfdx-hardis badge tooltip

--- src/components/BannerForm.js
+++ src/components/BannerForm.js
@@ -481,8 +481,7 @@ const BannerForm = ({ onSubmit, setMainError, onValidationError, onGenerateStart
               </span>
             </label>
             <Tooltip id='sfdx-hardis-badge-tooltip' place='top' delayShow={200} className='react-tooltip'>
-              Shows your highest verified training badge for sfdx-hardis by Cloudity. This is a course badge, not a
-              certification.
+              Shows your highest verified training badge for sfdx-hardis by Cloudity.
             </Tooltip>
           </fieldset>
           <fieldset>
